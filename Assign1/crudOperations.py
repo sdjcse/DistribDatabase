61a90ae505cc6266adbe0e18a9e926ca6b4433d7
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-01-28 13:39:56 -0700

    code cleanip

--- Assign1/crudOperations.py
+++ Assign1/crudOperations.py
@@ -1,5 +1,3 @@
-import psycopg2
-from dbConnector import dbConnector
 from Interface import *
 
 def alterAfterLoading(openconnection,ratingstablename):
@@ -29,57 +27,12 @@ def createTableForLoading(openconnection,ratingstablename):
     cursor.close()
     openconnection.commit()
 
-def readIt(tableName,value,columnName,isAll,selectColumns):
-    try:
-        con = dbConnector()
-        cursor = con.cursor()
-        if isAll:
-            queryString = "SELECT * "
-        else:
-            queryString = "SELECT "
-            for item in selectColumns:
-                queryString = queryString + item + ","
-            queryString = queryString[:-1]
-        queryString += ' FROM ' + tableName
-        cursor.execute(queryString)
-        records = cursor.fetchall()
-    except psycopg2.DatabaseError, e:
-        print 'Error %s' %e
-    finally:
-        con.close()
-    return records
-
-def insertAllVals(tableName,valueList):
-    try:
-        con = dbConnector()
-        cursor = con.cursor()
-        queryString = "INSERT INTO moviedetails VALUES (%s,%s,%s,%s);"
-        cursor.execute(queryString,tuple(valueList))
-        con.commit()
-    except psycopg2.DatabaseError, e:
-        print 'Error %s' %e
-    finally:
-        con.close()
 
 def bulkInsert(fileName,tableName,openConnection):
     cursor = openConnection.cursor()
     cursor.copy_from(open(fileName),tableName,sep=':')
     openConnection.commit()
 
-def copyTable(newTableName,oldTable,columns):
-    try:
-        con = dbConnector()
-        cursor = con.cursor()
-        queryString = "CREATE TABLE " + newTableName
-        queryString += " AS SELECT "
-        for element in columns:
-            queryString += element + ","
-        queryString = queryString[:-1]
-        queryString += " " + oldTable
-    except psycopg2.DatabaseError, e:
-        print 'Error %s' % e
-    finally:
-        con.close()
 
 def countData(openConnection,ratingsTableName):
     cursor = openConnection.cursor()
